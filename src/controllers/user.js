69b605f5c4113857829d77b2f56c4b991db2a037
Armen Juhl <armenjuhl@gmail.com>
2020-08-01 21:11:31 -0700

    add update and delete user functionality

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const {userById} = require('../controllers/auth');
+const _ = require('lodash');
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -32,4 +33,38 @@ exports.allUsers = (req, res) => {
     }
     res.json({ users });
   }).select("name email updated created")
+};
+
+exports.getUser = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
+
+exports.updateUser = (req, res, next) => {
+  let user = req.profile;
+  user = _.extend(user, req.body); // mutate the source object
+  user.updated = Date.now();
+  user.save(err => {
+    if (err) {
+     return res.status(400).json({
+       error: "You are not authorized to perform this action"
+     });
+    }
+    user.hashed_password = undefined;
+    user.salt = undefined;
+    res.json({user})
+  });
+};
+
+exports.deleteUser = (req, res, next) => {
+  let user = req.profile;
+  user.remove((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        error: err
+      });
+    }
+    res.json({message: "User deleted successfully"});
+  });
 };
